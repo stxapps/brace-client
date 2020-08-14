234b03c67f9beec9eb24f2550edb193f654689b2
wit <witwitchayakarn@gmail.com>
2020-08-14 12:28:10 +0700

    Big logo be the first in manifest.json in web for Blockstack Browser

--- packages/web/src/components/Loading.js
+++ packages/web/src/components/Loading.js
@@ -4,7 +4,7 @@ import logo from '../images/logo-short.svg';
 const Loading = () => {
   return (
     <div className="relatvie w-screen h-screen">
-      <div style={{ top: '33.3333%' }} className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-12 h-12 square-spin">
+      <div style={{ top: '33.3333%' }} className="absolute left-1/2 flex justify-center items-center w-12 h-12 transform -translate-x-1/2 -translate-y-1/2 square-spin">
         <img src={logo} alt="" />
       </div>
     </div>
