fe49d28448d0227f66f0af5dfc163298335d1a4a
witwitchayakarn <witwitchayakarn@gmail.com>
2020-09-03 18:00:02 +0700

    ShareViewController extends UIViewController instead of SLComposeServiceViewController

--- packages/mobile/ios/Save to Brace/ShareViewController.swift
+++ packages/mobile/ios/Save to Brace/ShareViewController.swift
@@ -1,8 +1,7 @@
 import UIKit
-import Social
 import MobileCoreServices
 
-class ShareViewController: SLComposeServiceViewController {
+class ShareViewController: UIViewController {
 
     let APP_DOMAIN_NAME = "bracedotto://app"
     let SAVE_TO_BRACE = "/save-to-brace"
@@ -10,41 +9,22 @@ class ShareViewController: SLComposeServiceViewController {
     let urlContentType = kUTTypeURL as String
     var sharedUrls: [String] = []
 
-    override func isContentValid() -> Bool {
-        guard let context = self.extensionContext else {
-            return false
-        }
-
-        let inputItems = context.inputItems
-        for (_, _inputItem) in inputItems.enumerated() {
-            guard let inputItem = _inputItem as? NSExtensionItem else {
-                return false
-            }
-
-            guard let attachments = inputItem.attachments else {
-                return false
-            }
-
-            for (_, attachment) in attachments.enumerated() {
-                if !attachment.hasItemConformingToTypeIdentifier(urlContentType) {
-                    return false
-                }
-            }
-        }
-
-        return true
-    }
-
-    override func didSelectPost() {
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
         let inputItems = self.extensionContext!.inputItems
+        guard inputItems.count > 0 else {
+            self.cancelRequest()
+            return
+        }
+
         for (i, _inputItem) in inputItems.enumerated() {
             guard let inputItem = _inputItem as? NSExtensionItem else {
                 self.cancelRequest()
                 return
             }
 
-            guard let attachments = inputItem.attachments else {
+            guard let attachments = inputItem.attachments, attachments.count > 0 else {
                 self.cancelRequest()
                 return
             }
@@ -56,26 +36,28 @@ class ShareViewController: SLComposeServiceViewController {
                 }
 
                 handleUrl(attachment: attachment,
-                              isLast: i == inputItems.count - 1 && j == attachments.count - 1)
+                          isLast: i == inputItems.count - 1 && j == attachments.count - 1)
             }
         }
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
-    }
-
     private func handleUrl (attachment: NSItemProvider, isLast: Bool) {
         attachment.loadItem(forTypeIdentifier: urlContentType,
-                                      options: nil) { [weak self] data, error in
-            if error == nil, let item = data as? URL, let this = self {
-                this.sharedUrls.append(item.absoluteString)
-                if isLast {
-                    this.completeRequest()
-                }
-            } else {
-                self!.cancelRequest()
+                            options: nil) { [unowned self] data, error in
+
+            guard error == nil else {
+                self.cancelRequest()
+                return
+            }
+
+            guard let url = data as? URL else {
+                self.cancelRequest()
+                return
+            }
+
+            self.sharedUrls.append(url.absoluteString)
+            if isLast {
+                self.completeRequest()
             }
         }
     }
@@ -88,28 +70,33 @@ class ShareViewController: SLComposeServiceViewController {
         }
 
         let sharedUrls = self.sharedUrls.joined(separator: "")
-        guard let url = URL(string: "\(APP_DOMAIN_NAME)\(SAVE_TO_BRACE)?addingUrls=\(sharedUrls)") else {
+        guard let url = URL(string: "\(APP_DOMAIN_NAME)\(SAVE_TO_BRACE)?text=\(sharedUrls)") else {
             self.cancelRequest()
             return
         }
+        
+        self.extensionContext!.open(url, completionHandler: nil)
+        
+        var responder = self as UIResponder?
+        let selectorOpenURL = sel_registerName("openURL:")
 
-        self.extensionContext!.open(url, completionHandler: { completed in
-
-            if !completed {
-                self.cancelRequest()
-                return
+        while (responder != nil) {
+            if responder!.responds(to: selectorOpenURL) {
+                responder!.perform(selectorOpenURL, with: url)
+                break
             }
 
-            self.extensionContext!.completeRequest(returningItems: [],
-                                                completionHandler: nil)
-        })
+            responder = responder!.next
+        }
+
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
 
     private func cancelRequest() {
 
         let alert = UIAlertController(title: "Invalid link!",
-                                    message: "Only URL links can be saved.",
-                             preferredStyle: .alert)
+                                      message: "Only URL links can be saved.",
+                                      preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Close", style: .cancel) { _ in
             self.dismiss(animated: true, completion: nil)
@@ -119,7 +106,7 @@ class ShareViewController: SLComposeServiceViewController {
         present(alert, animated: true, completion: nil)
 
         self.extensionContext!.cancelRequest(withError: NSError(domain:"BRACEDOTTO_ERROR",
-                                                                  code: 0,
-                                                              userInfo: nil))
+                                                                code: 0,
+                                                                userInfo: nil))
     }
 }
