b7441c120aa602685dab974b5bfada5bcb35fc43
wit <witwitchayakarn@gmail.com>
2020-11-10 17:03:31 +0700

    Sync from mobile to web

--- packages/web/src/wdyr.js
+++ packages/web/src/wdyr.js
@@ -3,6 +3,10 @@ import React from 'react';
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
-    trackAllPureComponents: true,
+    include: [/^((?!(MenuOptions|MenuTrigger)).)*$/],
+    exclude: [
+      /^Connect/, /^LogBox/,
+      /^AnimatedComponentWrapper$/, /^AnimatedComponent$/, /^Svg$/, /^G$/, /^Path$/,
+    ],
   });
 }
