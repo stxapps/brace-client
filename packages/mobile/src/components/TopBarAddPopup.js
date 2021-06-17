bf455adfb3a30c110d3099685b694bdb15943b48
wit <witwitchayakarn@gmail.com>
2021-06-17 14:20:54 +0700

    Set keyboard type to url in iOS

--- packages/mobile/src/components/TopBarAddPopup.js
+++ packages/mobile/src/components/TopBarAddPopup.js
@@ -104,7 +104,7 @@ class TopBarAddPopup extends React.PureComponent {
         <View style={tailwind('flex-row justify-start items-center')}>
           <Text style={tailwind('flex-none text-sm font-medium text-gray-700')}>Url:</Text>
           {/* onKeyPress event for Enter key only if there is multiline TextInput */}
-          <TextInput onChange={this.onAddInputChange} onSubmitEditing={this.onAddInputKeyPress} style={tailwind('ml-3 px-4 py-2 flex-1 bg-white text-base text-gray-900 font-normal border border-gray-500 rounded-full')} placeholder="https://" value={url} autoCapitalize="none" autoCompleteType="off" autoCorrect={false} autoFocus />
+          <TextInput onChange={this.onAddInputChange} onSubmitEditing={this.onAddInputKeyPress} style={tailwind('ml-3 px-4 py-2 flex-1 bg-white text-base text-gray-900 font-normal border border-gray-500 rounded-full')} placeholder="https://" value={url} autoCapitalize="none" autoCompleteType="off" autoCorrect={false} keyboardType="url" autoFocus />
         </View>
         {msg === '' ? <View style={tailwind('w-full h-3')} /> : <Text style={tailwind('pt-3 text-base text-red-500 font-normal')}>{msg}</Text>}
         <View style={tailwind('pt-3 flex-row justify-start items-center')}>
