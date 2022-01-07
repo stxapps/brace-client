fd58353c385e1f01603b1f28ef3727506f19e347
witwitchayakarn <witwitchayakarn@gmail.com>
2022-01-07 12:39:49 +0700

    Fix incorrect import description

--- packages/web/src/components/SettingsPopupData.js
+++ packages/web/src/components/SettingsPopupData.js
@@ -104,7 +104,7 @@ const _SettingsPopupDataImport = (props) => {
           <svg className="w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10ZM11 6C11 6.55228 10.5523 7 10 7C9.44772 7 9 6.55228 9 6C9 5.44772 9.44772 5 10 5C10.5523 5 11 5.44772 11 6ZM9 9C8.44772 9 8 9.44772 8 10C8 10.5523 8.44772 11 9 11V14C9 14.5523 9.44772 15 10 15H11C11.5523 15 12 14.5523 12 14C12 13.4477 11.5523 13 11 13V10C11 9.44772 10.5523 9 10 9H9Z" />
           </svg>
-          <p className="ml-1 text-base text-gray-500">No data to export.</p>
+          <p className="ml-1 text-base text-gray-500">No data to import.</p>
         </div>
         <p className="text-base text-gray-500">{importAllDataProgress.done} / {importAllDataProgress.total}</p>
       </div>
