507e17622b3a54c71719c0095d682d901759ef72
witwitchayakarn <witwitchayakarn@gmail.com>
2022-02-10 13:54:17 +0700

    Remove unused BlockstackAuth.js

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -12,54 +12,41 @@ import './stylesheets/tailwind.css';
 import './stylesheets/loading.css';
 import './stylesheets/patterns.css';
 
-import { BLOCKSTACK_AUTH } from './types/const';
 import reducers from './reducers';
 import { init } from './actions';
 import { queue, discard, effect } from './apis/customOffline';
-import { getUrlPathQueryHash } from './utils';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import App from './components/App';
-import BlockstackAuth from './components/BlockstackAuth';
 
-if (getUrlPathQueryHash(window.location.href).startsWith(BLOCKSTACK_AUTH.slice(1))) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BlockstackAuth />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-} else {
+offlineConfig.queue = queue;
+offlineConfig.discard = discard;
+offlineConfig.effect = effect;
+offlineConfig.persistCallback = () => {
+  init(store);
+};
+offlineConfig.dispatch = (...args) => {
+  store.dispatch(...args);
+};
 
-  offlineConfig.queue = queue;
-  offlineConfig.discard = discard;
-  offlineConfig.effect = effect;
-  offlineConfig.persistCallback = () => {
-    init(store);
-  };
-  offlineConfig.dispatch = (...args) => {
-    store.dispatch(...args);
-  };
-
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
     /** @type {any} */(reducers),
-    composeEnhancers(
-      installReduxLoop({ ENABLE_THUNK_MIGRATION: true }),
-      offline(offlineConfig),
-    )
-  );
+  composeEnhancers(
+    installReduxLoop({ ENABLE_THUNK_MIGRATION: true }),
+    offline(offlineConfig),
+  )
+);
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
