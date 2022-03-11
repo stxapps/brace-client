7dbf22c3d21d6e9bdc951243c37e0f1a26ec9d95
witwitchayakarn <witwitchayakarn@gmail.com>
2022-03-11 16:17:36 +0700

    Open a new tab for getting a support

--- packages/web/src/components/SettingsErrorPopup.js
+++ packages/web/src/components/SettingsErrorPopup.js
@@ -30,7 +30,7 @@ const SettingsErrorPopup = () => {
           </div>
           <div className="ml-3 lg:mt-0.5">
             <h3 className="text-base text-red-800 font-medium text-left lg:text-sm">Updating Settings Error!</h3>
-            <p className="mt-2.5 text-sm text-red-700">Please wait a moment and try again. <br className="hidden sm:inline" />If the problem persists, please <a className="underline rounded hover:text-red-800 focus:outline-none focus:ring-2 focus:ring-red-700" href={'/' + HASH_SUPPORT}>contact us
+            <p className="mt-2.5 text-sm text-red-700">Please wait a moment and try again. <br className="hidden sm:inline" />If the problem persists, please <a className="underline rounded hover:text-red-800 focus:outline-none focus:ring-2 focus:ring-red-700" href={'/' + HASH_SUPPORT} target="_blank" rel="noreferrer">contact us
               <svg className="mb-2 inline-block w-4" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11 3C10.4477 3 10 3.44772 10 4C10 4.55228 10.4477 5 11 5H13.5858L7.29289 11.2929C6.90237 11.6834 6.90237 12.3166 7.29289 12.7071C7.68342 13.0976 8.31658 13.0976 8.70711 12.7071L15 6.41421V9C15 9.55228 15.4477 10 16 10C16.5523 10 17 9.55228 17 9V4C17 3.44772 16.5523 3 16 3H11Z" />
                 <path d="M5 5C3.89543 5 3 5.89543 3 7V15C3 16.1046 3.89543 17 5 17H13C14.1046 17 15 16.1046 15 15V12C15 11.4477 14.5523 11 14 11C13.4477 11 13 11.4477 13 12V15H5V7H8C8.55228 7 9 6.55228 9 6C9 5.44772 8.55228 5 8 5H5Z" />
