75306ea3160e9f2415d49df01fcf59d979bbdfcf
wit <witwitchayakarn@gmail.com>
2024-08-27 09:57:28 +0700

    Add exclude service-worker.js and index.html in deploy scripts

--- packages/web/bin/deploy-test.sh
+++ packages/web/bin/deploy-test.sh
@@ -9,7 +9,7 @@ aws s3 sync build/.well-known s3://test-brace-web/.well-known --size-only --dele
 cachedItems=(logo16.png logo32.png logo48.png logo64.png logo192.png logo192-maskable.png logo512.png logo512-maskable.png logo-for-stacks-access.png ss-mobile-1.png ss-mobile-2.png ss-mobile-3.png ss-mobile-4.png ss-wide-1.png ss-wide-2.png ss-wide-3.png ss-wide-4.png twitter-card-image-pattern2.png twitter-card-image-pattern5.png)
 aws s3 sync build s3://test-brace-web --exclude "*" "${cachedItems[@]/#/--include=}" --size-only --delete --acl public-read --cache-control max-age=604800
 
-aws s3 sync build s3://test-brace-web --exclude "static/*" --exclude ".well-known/*" "${cachedItems[@]/#/--exclude=}" --size-only --delete --acl public-read
+aws s3 sync build s3://test-brace-web --exclude "static/*" --exclude ".well-known/*" "${cachedItems[@]/#/--exclude=}" --exclude service-worker.js --exclude index.html --size-only --delete --acl public-read
 
 aws s3 cp build/service-worker.js s3://test-brace-web/service-worker.js --acl public-read --cache-control no-cache
 aws s3 cp build/index.html s3://test-brace-web/index.html --acl public-read --cache-control no-cache
