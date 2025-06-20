9671907af0d26fc67f58e57fd1b369f21aae223b
wit <1229910+0xc22b@users.noreply.github.com>
2025-06-20 17:41:42 +0700

    Fix incorrect width under KeyboardAvoidingView

--- packages/mobile/src/components/CustomEditorPopup.js
+++ packages/mobile/src/components/CustomEditorPopup.js
@@ -204,8 +204,8 @@ const CustomEditorPopup = () => {
       <TouchableWithoutFeedback>
         <Animated.View style={[tailwind('absolute inset-0 bg-black bg-opacity-25'), bgStyle]} />
       </TouchableWithoutFeedback>
-      <View style={tailwind(`flex-1 items-center justify-center p-4 ${Platform.OS === 'ios' ? 'lg:justify-start' : ''}`)}>
-        <KeyboardAvoidingView behavior="padding">
+      <View style={tailwind(`flex-1 justify-center p-4 ${Platform.OS === 'ios' ? 'lg:justify-start' : ''}`)}>
+        <KeyboardAvoidingView behavior="padding" style={tailwind('items-center')}>
           <Animated.View style={[tailwind('w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-xl blk:border blk:border-gray-700 blk:bg-gray-800'), popupStyle]}>
             <ScrollView style={{ maxHeight: panelHeight }} keyboardShouldPersistTaps="handled">
               {imageUrl && <View style={tailwind('w-full rounded-t-lg bg-white blk:border-b blk:border-gray-700 blk:bg-gray-800 aspect-7/12 shadow-xs')}>
--- packages/mobile/src/components/TagEditorPopup.js
+++ packages/mobile/src/components/TagEditorPopup.js
@@ -249,8 +249,8 @@ const TagEditorPopup = () => {
       <TouchableWithoutFeedback>
         <Animated.View style={[tailwind('absolute inset-0 bg-black bg-opacity-25'), bgStyle]} />
       </TouchableWithoutFeedback>
-      <View style={tailwind(`flex-1 items-center justify-center p-4 ${Platform.OS === 'ios' ? 'lg:justify-start' : ''}`)}>
-        <KeyboardAvoidingView behavior="padding">
+      <View style={tailwind(`flex-1 justify-center p-4 ${Platform.OS === 'ios' ? 'lg:justify-start' : ''}`)}>
+        <KeyboardAvoidingView behavior="padding" style={tailwind('items-center')}>
           <Animated.View style={[tailwind('w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-xl blk:border blk:border-gray-700 blk:bg-gray-800'), popupStyle]}>
             <ScrollView style={{ maxHeight: panelHeight }} keyboardShouldPersistTaps="handled">
               <View style={tailwind('px-4 pt-8 pb-4 sm:px-6 sm:pb-6')}>
