25a1ccb6753d69be0f5b1078ec0c3d7de6fb32da
wit <witwitchayakarn@gmail.com>
2025-06-23 16:25:14 +0700

    Handle keyboard separately btw iOS and Android for BottomBarSearchPopup

--- packages/mobile/src/components/BottomBarSearchPopup.js
+++ packages/mobile/src/components/BottomBarSearchPopup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   View, Text, TouchableOpacity, TextInput, Animated, Keyboard, BackHandler, Platform,
-  Easing,
+  KeyboardAvoidingView,
 } from 'react-native';
 import { connect } from 'react-redux';
 import Svg, { Path } from 'react-native-svg';
@@ -11,7 +11,7 @@ import {
   SEARCH_POPUP, BOTTOM_BAR_HEIGHT, SEARCH_POPUP_HEIGHT, BLK_MODE,
 } from '../types/const';
 import { getThemeMode } from '../selectors';
-import { isNumber, toPx } from '../utils';
+import { toPx } from '../utils';
 import { bbFMV } from '../types/animConfigs';
 
 import { withTailwind } from '.';
@@ -22,7 +22,7 @@ class BottomBarSearchPopup extends React.PureComponent {
     super(props);
 
     this.state = {
-      keyboardHeight: 0, keyboardDuration: 0,
+      keyboardHeight: 0,
     };
 
     this.searchInput = React.createRef();
@@ -111,14 +111,12 @@ class BottomBarSearchPopup extends React.PureComponent {
     if (isSearchPopupShown) {
       if (!this.keyboardDidShowListener) {
         this.keyboardDidShowListener = Keyboard.addListener('keyboardWillShow', (e) => {
-          this.setState({
-            keyboardHeight: e.endCoordinates.height, keyboardDuration: e.duration,
-          });
+          this.setState({ keyboardHeight: e.endCoordinates.height });
         });
       }
       if (!this.keyboardDidHideListener) {
-        this.keyboardDidHideListener = Keyboard.addListener('keyboardWillHide', (e) => {
-          this.setState({ keyboardHeight: 0, keyboardDuration: e.duration });
+        this.keyboardDidHideListener = Keyboard.addListener('keyboardWillHide', () => {
+          this.setState({ keyboardHeight: 0 });
         });
       }
     } else {
@@ -136,7 +134,7 @@ class BottomBarSearchPopup extends React.PureComponent {
   translateSearchPopup = (prevIsBottomBarShown) => {
     const { isBottomBarShown, isSearchPopupShown, insets } = this.props;
 
-    let toValue, isIosAnim = false;
+    let toValue;
     if (!isBottomBarShown) {
       toValue = toPx(BOTTOM_BAR_HEIGHT) + toPx(SEARCH_POPUP_HEIGHT) + insets.bottom;
     } else {
@@ -145,8 +143,7 @@ class BottomBarSearchPopup extends React.PureComponent {
       } else {
         const bottom = toPx(BOTTOM_BAR_HEIGHT) + insets.bottom;
         if (Platform.OS === 'ios' && this.state.keyboardHeight > bottom) {
-          toValue = -1 * (this.state.keyboardHeight - bottom);
-          isIosAnim = true;
+          toValue = toPx(BOTTOM_BAR_HEIGHT) + insets.bottom;
         } else {
           toValue = 0;
         }
@@ -155,13 +152,6 @@ class BottomBarSearchPopup extends React.PureComponent {
 
     if (!prevIsBottomBarShown && isBottomBarShown) {
       Animated.timing(this.searchPopupTranslateY, { toValue, ...bbFMV.visible }).start();
-    } else if (isIosAnim) {
-      let duration = this.state.keyboardDuration;
-      if (!isNumber(duration)) duration = 250;
-
-      Animated.timing(this.searchPopupTranslateY, {
-        toValue, duration, easing: Easing.inOut(Easing.ease), useNativeDriver: true,
-      }).start();
     } else {
       Animated.timing(this.searchPopupTranslateY, {
         toValue: toValue,
@@ -187,8 +177,32 @@ class BottomBarSearchPopup extends React.PureComponent {
     Keyboard.dismiss();
   };
 
+  renderContent() {
+    const { searchString, themeMode, tailwind } = this.props;
+
+    const searchClearBtnClasses = searchString.length === 0 ? 'hidden relative' : 'flex absolute';
+    const inputClassNames = Platform.OS === 'ios' ? 'py-2 leading-4' : 'py-0.5';
+
+    return (
+      <>
+        <View style={tailwind('flex-shrink flex-grow')}>
+          <TextInput ref={this.searchInput} onChange={this.onSearchInputChange} style={tailwind(`w-full rounded-full border border-gray-400 bg-white pl-4 pr-6 text-sm font-normal text-gray-700 blk:border-gray-600 blk:bg-gray-800 blk:text-gray-200 ${inputClassNames}`)} placeholder="Search" placeholderTextColor={themeMode === BLK_MODE ? 'rgb(156, 163, 175)' : 'rgb(107, 114, 128)'} value={searchString} autoCapitalize="none" />
+          {/* A bug display: none doesn't work with absolute, need to change to relative. https://github.com/facebook/react-native/issues/18415 */}
+          <TouchableOpacity onPress={this.onSearchClearBtnClick} style={tailwind(`inset-y-0 right-0 items-center justify-center pr-2 ${searchClearBtnClasses}`)}>
+            <Svg style={tailwind('font-normal text-gray-400 blk:text-gray-400')} width={20} height={20} viewBox="0 0 20 20" fill="currentColor">
+              <Path fillRule="evenodd" clipRule="evenodd" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM8.70711 7.29289C8.31658 6.90237 7.68342 6.90237 7.29289 7.29289C6.90237 7.68342 6.90237 8.31658 7.29289 8.70711L8.58579 10L7.29289 11.2929C6.90237 11.6834 6.90237 12.3166 7.29289 12.7071C7.68342 13.0976 8.31658 13.0976 8.70711 12.7071L10 11.4142L11.2929 12.7071C11.6834 13.0976 12.3166 13.0976 12.7071 12.7071C13.0976 12.3166 13.0976 11.6834 12.7071 11.2929L11.4142 10L12.7071 8.70711C13.0976 8.31658 13.0976 7.68342 12.7071 7.29289C12.3166 6.90237 11.6834 6.90237 11.2929 7.29289L10 8.58579L8.70711 7.29289Z" />
+            </Svg>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity onPress={this.onSearchCancelBtnClick} style={tailwind('ml-2 h-10 flex-shrink-0 flex-grow-0 items-center justify-center rounded-md px-1.5 py-1')}>
+          <Text style={tailwind('text-sm font-normal text-gray-500 blk:text-gray-300')}>Cancel</Text>
+        </TouchableOpacity>
+      </>
+    );
+  }
+
   render() {
-    const { searchString, insets, themeMode, tailwind } = this.props;
+    const { isSearchPopupShown, insets, tailwind } = this.props;
 
     // Only transition when moving with BottomBar
     //   but when show/hide this search popup, no need animation
@@ -198,27 +212,21 @@ class BottomBarSearchPopup extends React.PureComponent {
       paddingLeft: insets.left, paddingRight: insets.right,
       transform: [{ translateY: this.searchPopupTranslateY }],
     };
-    const searchClearBtnClasses = searchString.length === 0 ? 'hidden relative' : 'flex absolute';
-    const inputClassNames = Platform.OS === 'ios' ? 'py-2 leading-4' : 'py-0.5';
 
-    return (
+    const popup = Platform.OS === 'ios' ? (
+      <KeyboardAvoidingView behavior="position" enabled={isSearchPopupShown} style={tailwind('absolute inset-x-0 bottom-0 z-10')}>
+        <Animated.View style={[tailwind('absolute inset-x-0 bg-white blk:bg-gray-800'), style]}>
+          {this.renderContent()}
+        </Animated.View>
+      </KeyboardAvoidingView>
+    ) : (
       <Animated.View style={[tailwind('absolute inset-x-0 z-10 bg-white blk:bg-gray-800'), style]}>
         <View style={tailwind('flex-row items-center justify-between border-t border-gray-200 px-2 py-2 blk:border-gray-700')}>
-          <View style={tailwind('flex-shrink flex-grow')}>
-            <TextInput ref={this.searchInput} onChange={this.onSearchInputChange} style={tailwind(`w-full rounded-full border border-gray-400 bg-white pl-4 pr-6 text-sm font-normal text-gray-700 blk:border-gray-600 blk:bg-gray-800 blk:text-gray-200 ${inputClassNames}`)} placeholder="Search" placeholderTextColor={themeMode === BLK_MODE ? 'rgb(156, 163, 175)' : 'rgb(107, 114, 128)'} value={searchString} autoCapitalize="none" />
-            {/* A bug display: none doesn't work with absolute, need to change to relative. https://github.com/facebook/react-native/issues/18415 */}
-            <TouchableOpacity onPress={this.onSearchClearBtnClick} style={tailwind(`inset-y-0 right-0 items-center justify-center pr-2 ${searchClearBtnClasses}`)}>
-              <Svg style={tailwind('font-normal text-gray-400 blk:text-gray-400')} width={20} height={20} viewBox="0 0 20 20" fill="currentColor">
-                <Path fillRule="evenodd" clipRule="evenodd" d="M10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18ZM8.70711 7.29289C8.31658 6.90237 7.68342 6.90237 7.29289 7.29289C6.90237 7.68342 6.90237 8.31658 7.29289 8.70711L8.58579 10L7.29289 11.2929C6.90237 11.6834 6.90237 12.3166 7.29289 12.7071C7.68342 13.0976 8.31658 13.0976 8.70711 12.7071L10 11.4142L11.2929 12.7071C11.6834 13.0976 12.3166 13.0976 12.7071 12.7071C13.0976 12.3166 13.0976 11.6834 12.7071 11.2929L11.4142 10L12.7071 8.70711C13.0976 8.31658 13.0976 7.68342 12.7071 7.29289C12.3166 6.90237 11.6834 6.90237 11.2929 7.29289L10 8.58579L8.70711 7.29289Z" />
-              </Svg>
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity onPress={this.onSearchCancelBtnClick} style={tailwind('ml-2 h-10 flex-shrink-0 flex-grow-0 items-center justify-center rounded-md px-1.5 py-1')}>
-            <Text style={tailwind('text-sm font-normal text-gray-500 blk:text-gray-300')}>Cancel</Text>
-          </TouchableOpacity>
-        </View>
-      </Animated.View>
+          {this.renderContent()}
+        </View >
+      </Animated.View >
     );
+    return popup;
   }
 }
 
