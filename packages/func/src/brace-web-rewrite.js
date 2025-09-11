3ecd1fffaf6887717b9a13f0b70bcd674bf2fcc0
wit <witwitchayakarn@gmail.com>
2025-09-11 17:53:12 +0700

    Add CloudFront functions

--- packages/func/src/brace-web-rewrite.js
+++ packages/func/src/brace-web-rewrite.js
@@ -1,81 +1,51 @@
 /**
- * This function handles viewer requests for a hybrid MPA/SPA site.
+ * This function handles viewer requests for a hybrid site
+ *   that supports clean page URLs and bookmarking via path.
  * 1. Redirects www to non-www.
  * 2. Pass through the root path.
- * 3. Pass through asset paths
- * 4. Redirects clean paths with trailing slashes to their canonical non-slash version.
- * 5. Rewrites clean paths with no dots with .html.
- * 6. Rewrites paths with a valid url format to /404.html.
- * 7. Redirects paths with trailing slashes to their canonical non-slash version.
- * 8. Pass through paths with dot.
- * 9. Rewrites no-dot paths with .html.
+ * 3. Pass through paths with dots:
+ *    - This handles /favicon.ico, /example.com, /example.com/some/path, etc.
+ *    - BUG ALERT: No support directory names with a dot!
+ * 4. For page paths (no dot in URI) e.g., /support or /support/:
+ *    - Redirects paths with a trailing slash to the non-slash version.
+ *    - Rewrites clean paths to their .html equivalent.
  *
  * @param {AWSCloudFrontFunction.Event} event - The CloudFront event object.
  * @returns {AWSCloudFrontFunction.Request | AWSCloudFrontFunction.Response}
  */
 function handler(event) {
   const request = event.request;
+  const host = request.headers.host.value;
   const uri = request.uri;
 
-  // 1.
-  if (request.headers.host.value === 'www.brace.to') {
+  // 1. Redirect www to non-www.
+  if (host.startsWith('www.')) {
     return {
       statusCode: 301,
       statusDescription: 'Moved Permanently',
       headers: {
-        location: { value: `https://brace.to${uri}` },
+        location: { value: `https://${host.slice(4)}${uri}` },
       },
     };
   }
 
-  // 2.
+  // 2. Pass through the root path.
   if (uri === '' || uri === '/') return request;
 
-  // 3.
-  let urlPattern = /^\/(.+\/)*.+\..+$/;
-  if (urlPattern.test(uri)) return request;
-
-  // 4.
-  urlPattern = /^\/(\w+\/)*\w+\/+$/;
-  if (urlPattern.test(uri)) {
-    return {
-      statusCode: 301,
-      statusDescription: 'Moved Permanently',
-      headers: {
-        location: { value: uri.slice(0, -1) },
-      },
-    };
-  }
-
-  // 5.
-  urlPattern = /^\/(\w+\/)*\w+$/;
-  if (urlPattern.test(uri)) {
-    request.uri += '.html';
-    return request;
-  }
-
-  // 6.
-  urlPattern = /^(https?:\/\/)?([a-zA-Z0-9-]{1,256}\.)+[a-zA-Z]{2,8}/;
-  if (urlPattern.test(uri)) {
-    request.uri = '/404.html';
-    return request;
-  }
-
-  // 7.
-  if (uri.length > 1 && uri.endsWith('/')) {
-    return {
-      statusCode: 301,
-      statusDescription: 'Moved Permanently',
-      headers: {
-        location: { value: uri.slice(0, -1) },
-      },
-    };
-  }
-
-  // 8.
+  // 3. Pass through paths with dots.
   if (uri.includes('.')) return request;
 
-  // 9.
+  // 4. Handle page paths (no dots in URI).
+  if (uri.endsWith('/')) {
+    return {
+      statusCode: 301,
+      statusDescription: 'Moved Permanently',
+      headers: {
+        location: { value: uri.slice(0, -1) },
+      },
+    };
+  }
+
   request.uri += '.html';
   return request;
 }
